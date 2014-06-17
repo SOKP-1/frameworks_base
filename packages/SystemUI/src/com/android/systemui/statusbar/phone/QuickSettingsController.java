68267f3c7e2457bab891a20d6fdb285049192d70
Alex-Cruz <mazdarider23@gmail.com>
2014-06-17 05:48:45 -0500

    App Circle Bar Toggle [1/2]

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
@@ -56,6 +56,7 @@ import static com.android.internal.util.cm.QSConstants.TILE_WIFIAP;
 import static com.android.internal.util.cm.QSConstants.TILE_WIMAX;
 import static com.android.internal.util.cm.QSConstants.TILE_POWER;
 import static com.android.internal.util.cm.QSConstants.TILE_THEME;
+import static com.android.internal.util.cm.QSConstants.TILE_APPCIRCLEBAR;
 
 import android.content.BroadcastReceiver;
 import android.content.ContentResolver;
@@ -119,6 +120,7 @@ import com.android.systemui.quicksettings.WiFiTile;
 import com.android.systemui.quicksettings.WifiAPTile;
 import com.android.systemui.quicksettings.PowerMenuTile;
 import com.android.systemui.quicksettings.OnTheGoTile;
+import com.android.systemui.quicksettings.AppcirclebarTile;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -316,6 +318,8 @@ public class QuickSettingsController {
             } else if (tile.contains(TILE_CPUFREQ)) {
                 if (cpufreqSupported) {
                     qs = new CPUFreqTile(mContext, this);
+            } else if (tile.equals(TILE_APPCIRCLEBAR)) {
+                qs = new AppcirclebarTile(mContext, this);
                 }
             }
 
